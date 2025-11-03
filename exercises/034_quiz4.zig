84f758265c84289e12b75594884c8ee15f335a9d
MevyDev <mevydev@proton.me>
2025-11-03 01:44:20 +0100

    034 zigzigzigzigzigzig

--- exercises/034_quiz4.zig
+++ exercises/034_quiz4.zig
@@ -9,12 +9,19 @@ const std = @import("std");
 
 const NumError = error{IllegalNumber};
 
-pub fn main() void {
+pub fn main() !void {
     var stdout = std.fs.File.stdout().writer(&.{});
 
-    const my_num: u32 = getNumber();
+    const my_num1: u32 = try getNumber();
+    const my_num2: u32 = getNumber() catch 0;
+    const my_num3: u32 = getNumber() catch {
+        std.debug.print("oh noooo :(\n", .{});
+        return;
+    };
 
-    try stdout.interface.print("my_num={}\n", .{my_num});
+    try stdout.interface.print("my_num={}\n", .{my_num1});
+    try stdout.interface.print("my_num={}\n", .{my_num2});
+    try stdout.interface.print("my_num={}\n", .{my_num3});
 }
 
 // This function is obviously weird and non-functional. But you will not be changing it for this quiz.
